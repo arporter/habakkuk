56ed40d99868c7fcf20cfae03af5d73cb6d174be
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-09-08 12:11:46 +0100

    #3 Remove unused eval_expr() code from parse2003

--- src/parse2003.py
+++ src/parse2003.py
@@ -1,33 +1,6 @@
 ''' Module containing classes related to parsing Fortran code using
     the f2003 parser '''
-import ast
-import operator as op
 
-# supported operators
-operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
-             ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
-             ast.USub: op.neg}
-
-def eval_expr(expr):
-    """
-    >>> eval_expr('2^6')
-    4
-    >>> eval_expr('2**6')
-    64
-    >>> eval_expr('1 + 2*3**(4^5) / (6 + -7)')
-    -5.0
-    """
-    return eval_(ast.parse(expr, mode='eval').body)
-
-def eval_(node):
-    if isinstance(node, ast.Num): # <number>
-        return node.n
-    elif isinstance(node, ast.BinOp): # <left> <operator> <right>
-        return operators[type(node.op)](eval_(node.left), eval_(node.right))
-    elif isinstance(node, ast.UnaryOp): # <operator> <operand> e.g., -1
-        return operators[type(node.op)](eval_(node.operand))
-    else:
-        raise TypeError(node)
 
 def walk(children, my_type, indent=0, debug=False):
     '''' Walk down the tree produced by the f2003 parser where children
