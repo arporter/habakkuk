3ebe75c9b358f1069c76c59f9ebfe58def587a49
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-09-09 16:07:55 +0100

    #3 Add further tests for calls to main()

--- src/tests/make_dag_test.py
+++ src/tests/make_dag_test.py
@@ -89,3 +89,39 @@ def test_main_routine_file_not_present_err():
     assert (
         "The specified source file ('not_a_file') does not exist" in str(err))
 
+
+def test_main_routine_valid_file():
+    ''' Test that we can run the main routine of Habakkuk and that we get
+    the expected dag written to file '''
+    args = ["test_files/basic_loop.f90"]
+    make_dag.main(args)
+    graph_file = os.path.join(PWD, "basic_loop_routine_loop1.gv")
+    assert os.path.isfile(graph_file)
+    with open(graph_file, "r") as fout:
+        graph = fout.read()
+    assert "label=\"2.0\", color=\"green\"" in graph
+    assert "label=\"i\", color=\"black\"" in graph
+    assert "label=\"*\", color=\"red\", shape=\"box\"" in graph
+    assert "label=\"aprod(i)\", color=\"blue\"" in graph
+
+
+def test_main_routine_prune_scalar_temporaries():
+    ''' Test that we can run the main routine of Habakkuk and request
+    that any nodes representing scalar temporaries be pruned from the dag
+    that is constructed '''
+    args = ["--rm-scalar-tmps", "test_files/two_different_duplicate_op.f90"]
+    make_dag.main(args)
+    graph_file = os.path.join(PWD, "test_duplicate_prod_div.gv")
+    assert os.path.isfile(graph_file)
+
+
+def test_main_routine_invalid_fortran():
+    ''' Check that we raise the expected error if given a source file that
+    is not valid Fortran '''
+    from parse2003 import ParseError
+    args = ["make_dag_test.py"]
+    with pytest.raises(ParseError) as err:
+        make_dag.main(args)
+    print str(err)
+    assert "Parse Error: Parsing " in str(err)
+    assert "Is the file valid Fortran?" in str(err)
