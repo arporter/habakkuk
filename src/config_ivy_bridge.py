c7fc6f986e32dd11ee9f2b8dbfc2fcc1f066c105
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-09-12 12:03:43 +0100

    #8 Add ** as a recognised OPERATOR. Costs are just a guess at the moment

--- src/config_ivy_bridge.py
+++ src/config_ivy_bridge.py
@@ -7,14 +7,18 @@
 # searching for duplicated sub-graphs).
 # TODO these costs are microarchitecture specific.
 OPERATORS = {"/": {"latency": 15, "cost": 8},
+             # TODO how is pow() evaluated on an Ivy Bridge?
+             "**": {"latency":15 , "cost": 8},
              "+": {"latency": 3, "cost": 1},
              "-": {"latency": 3, "cost": 1},
              "*": {"latency": 5, "cost": 1}}
 
 # Which execution port each f.p. operation is mapped to on the CPU
 # (from http://www.agner.org/optimize/microarchitecture.pdf).
+# I assume pow() will be evaluated as a series expansion involving
+# f.p. multiplications/divisions.
 NUM_EXECUTION_PORTS = 2
-CPU_EXECUTION_PORTS = {"/": 0, "*": 0, "+": 1, "-": 1}
+CPU_EXECUTION_PORTS = {"/": 0, "**": 0, "*": 0, "+": 1, "-": 1}
 
 # Size of a cache line in bytes
 CACHE_LINE_BYTES = 64
