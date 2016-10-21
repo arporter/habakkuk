26c4397028ce204493ddd4b1bdedb2cfa64e5b8a
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-10-21 15:01:55 +0100

    #15 add TRIM, NINT and SUM intrinsics

--- src/habakkuk/config_ivy_bridge.py
+++ src/habakkuk/config_ivy_bridge.py
@@ -20,7 +20,15 @@ OPERATORS = {"**": {"latency": 0, "cost": 75, "flops": 28},
              "-": {"latency": 3, "cost": 1, "flops": 1},
              "*": {"latency": 5, "cost": 1, "flops": 1},
              "MIN": {"latency": 0, "cost": 1, "flops": 0},
-             "MAX": {"latency": 0, "cost": 1, "flops": 0}}
+             "MAX": {"latency": 0, "cost": 1, "flops": 0},
+             # TODO how to handle an intrinsic when the cost depends
+             # on the argument?
+             "SUM": {"latency": 0, "cost": 1, "flops": 1},
+             # String intrinsics. Obviously these don't account
+             # for any FLOPs but need to be here so that we can
+             # grok the Fortran
+             "TRIM": {"latency": 0, "cost": 1, "flops": 0},
+             "NINT": {"latency": 0, "cost": 1, "flops": 0}}
 
 # Which execution port each f.p. operation is mapped to on the CPU
 # (from http://www.agner.org/optimize/microarchitecture.pdf).
@@ -28,9 +36,12 @@ NUM_EXECUTION_PORTS = 6
 CPU_EXECUTION_PORTS = {"/": 0, "*": 0, "+": 1, "-": 1,
                        # Which port the intrinsics will utilise
                        "**": 0, "SIN": 0, "COS": 0, "SIGN": 1, "EXP": 0,
+                       "SUM": 0,
                        # The CMP instruction can execute on 0, 1 or 5
                        # so specify 5 here as 0 and 1 are likely to be busy
-                       "MAX": 5, "MIN": 5}
+                       "MAX": 5, "MIN": 5,
+                       # String manipulation is integer
+                       "TRIM": 5, "NINT": 5}
 
 # Size of a cache line in bytes
 CACHE_LINE_BYTES = 64
@@ -39,7 +50,8 @@ CACHE_LINE_BYTES = 64
 EXAMPLE_CLOCK_GHZ = 3.85
 
 # Fortran intrinsics that we recognise. All uppercase.
-FORTRAN_INTRINSICS = ["SIGN", "SIN", "COS", "**", "MAX", "MIN", "EXP"]
+FORTRAN_INTRINSICS = ["SIGN", "SIN", "COS", "**", "MAX", "MIN", "EXP",
+                      "TRIM", "NINT", "SUM"]
 
 # Whether this microarchitecture supports the Fused Multiply Add op
 # TODO check on this before we attempt to generate FMAs.
