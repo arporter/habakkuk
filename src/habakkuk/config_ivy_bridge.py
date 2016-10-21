480e188c4a5b235b96a4fb67c660cce8b04a5293
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-10-21 14:13:01 +0100

    #15 Add place-holder for exp() fortran intrinsic

--- src/habakkuk/config_ivy_bridge.py
+++ src/habakkuk/config_ivy_bridge.py
@@ -10,6 +10,8 @@
 # run-time. Costs and FLOP-counts for these are obtained by running
 # micro-benchmarks (dl_microbench) using a tool such as likwid.
 OPERATORS = {"**": {"latency": 0, "cost": 75, "flops": 28},
+             # TODO measure actual values for EXP on ivy bridge
+             "EXP": {"latency": 0, "cost": 50, "flops": 20},
              "SIN": {"latency": 0, "cost": 49, "flops": 40},
              "COS": {"latency": 0, "cost": 49, "flops": 40},
              "/": {"latency": 15, "cost": 8, "flops": 1},
@@ -25,7 +27,7 @@ OPERATORS = {"**": {"latency": 0, "cost": 75, "flops": 28},
 NUM_EXECUTION_PORTS = 6
 CPU_EXECUTION_PORTS = {"/": 0, "*": 0, "+": 1, "-": 1,
                        # Which port the intrinsics will utilise
-                       "**": 0, "SIN": 0, "COS": 0, "SIGN": 1,
+                       "**": 0, "SIN": 0, "COS": 0, "SIGN": 1, "EXP": 0,
                        # The CMP instruction can execute on 0, 1 or 5
                        # so specify 5 here as 0 and 1 are likely to be busy
                        "MAX": 5, "MIN": 5}
@@ -37,7 +39,7 @@ CACHE_LINE_BYTES = 64
 EXAMPLE_CLOCK_GHZ = 3.85
 
 # Fortran intrinsics that we recognise. All uppercase.
-FORTRAN_INTRINSICS = ["SIGN", "SIN", "COS", "**", "MAX", "MIN"]
+FORTRAN_INTRINSICS = ["SIGN", "SIN", "COS", "**", "MAX", "MIN", "EXP"]
 
 # Whether this microarchitecture supports the Fused Multiply Add op
 # TODO check on this before we attempt to generate FMAs.
