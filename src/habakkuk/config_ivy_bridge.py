31fac67db1a1d217df6b7450f6fc8f8a7be705e9
Andrew Porter <andrew.porter@stfc.ac.uk>
2016-11-08 09:45:05 +0000

    #15 Add MOD, REAL and PRESENT to recognised intrinsics

--- src/habakkuk/config_ivy_bridge.py
+++ src/habakkuk/config_ivy_bridge.py
@@ -21,6 +21,7 @@ OPERATORS = {"**": {"latency": 0, "cost": 75, "flops": 28},
              "TAN": {"latency": 0, "cost": 49, "flops": 40},
              "TANH": {"latency": 0, "cost": 49, "flops": 40},
              "/": {"latency": 15, "cost": 8, "flops": 1},
+             "MOD": {"latency": 15, "cost": 8, "flops": 1},
              "SIGN": {"latency": 0, "cost": 3, "flops": 1},
              "ABS": {"latency": 0, "cost": 3, "flops": 1},
              "+": {"latency": 3, "cost": 1, "flops": 1},
@@ -30,9 +31,11 @@ OPERATORS = {"**": {"latency": 0, "cost": 75, "flops": 28},
              "MAX": {"latency": 0, "cost": 1, "flops": 0},
              "INT": {"latency": 0, "cost": 1, "flops": 0},
              "DBLE": {"latency": 0, "cost": 1, "flops": 0},
+             "REAL": {"latency": 0, "cost": 1, "flops": 0},
              # TODO how to handle an intrinsic when the cost depends
              # on the argument?
              "SUM": {"latency": 0, "cost": 1, "flops": 1},
+             "PRESENT": {"latency": 0, "cost": 1, "flops": 0},
              # String intrinsics. Obviously these don't account
              # for any FLOPs but need to be here so that we can
              # grok the Fortran
@@ -47,17 +50,18 @@ NUM_EXECUTION_PORTS = 6
 CPU_EXECUTION_PORTS = {"/": 0, "*": 0, "+": 1, "-": 1,
                        # Which port the intrinsics will utilise
                        "**": 0, "SIN": 0, "COS": 0, "ACOS": 0, "SIGN": 1,
-                       "EXP": 0, "LOG": 0,
+                       "EXP": 0, "LOG": 0, "MOD": 0,
                        "SUM": 0, "ATAN": 0, "TAN": 0, "TANH": 0, "SQRT": 0,
                        # The CMP instruction can execute on 0, 1 or 5
                        # so specify 5 here as 0 and 1 are likely to be busy
-                       "MAX": 5, "MIN": 5, "ABS": 5, "DBLE": 1,
+                       "MAX": 5, "MIN": 5, "ABS": 5, "PRESENT": 5,
+                       "DBLE": 1, "REAL": 1, "INT": 1, "NINT": 1,
                        # String manipulation is integer
                        # TODO handle string manipulation separately as
                        # it's not really something we want to have to
                        # care about when thinking about number-crunching
                        # performance
-                       "TRIM": 1, "INT": 1, "NINT": 1, "COUNT": 1, "IACHAR": 1}
+                       "TRIM": 1, "COUNT": 1, "IACHAR": 1}
 
 # Size of a cache line in bytes
 CACHE_LINE_BYTES = 64
@@ -69,8 +73,9 @@ FORTRAN_STRING_INTRINSICS = ["TRIM", "COUNT", "IACHAR"]
 
 # Fortran intrinsics that we recognise. All uppercase.
 FORTRAN_INTRINSICS = ["SIGN", "SIN", "COS", "ACOS", "**", "MAX", "MIN", "EXP",
-                      "NINT", "SUM", "ATAN", "TAN", "TANH", "SQRT", "ABS", "LOG",
-                      "INT", "DBLE"] + FORTRAN_STRING_INTRINSICS
+                      "MOD", "NINT", "SUM", "ATAN", "TAN", "TANH", "SQRT",
+                      "ABS", "LOG", "INT", "REAL", "DBLE", "PRESENT"] + \
+    FORTRAN_STRING_INTRINSICS
 
 # Whether this microarchitecture supports the Fused Multiply Add op
 # TODO check on this before we attempt to generate FMAs.
