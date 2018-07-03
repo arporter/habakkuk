dcffcedc8dbfc03b69c09d933b5b81c2bcab1052
Andrew Porter <andrew.porter@stfc.ac.uk>
2018-07-03 11:15:16 +0100

    #48 update cmd-line arg test

--- src/habakkuk/tests/cmd_line_test.py
+++ src/habakkuk/tests/cmd_line_test.py
@@ -1,6 +1,42 @@
+# -----------------------------------------------------------------------------
+# BSD 3-Clause License
+#
+# Copyright (c) 2017-2018, Science and Technology Facilities Council.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# -----------------------------------------------------------------------------
+# Author A. R. Porter, STFC Daresbury Lab
+
 ''' This module tests aspects of Habakkuk related to handling of
     command-line arguments '''
 
+from __future__ import absolute_import, print_function
 import os
 import subprocess
 
@@ -21,7 +57,7 @@ def test_usage_message():
     (output, _) = subprocess.Popen([HABAKKUK_SCRIPT],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.STDOUT).communicate()
-    assert "Usage: habakkuk [options] <Fortran files>" in output
+    assert "Usage: habakkuk [options] <Fortran file(s)>" in output
 
 
 def test_missing_file():
@@ -52,7 +88,6 @@ def test_help_message():
     assert (
         "Options:\n"
         "  -h, --help            show this help message and exit\n"
-        "  --task=TASK           Specify parsing result task. Default: show.\n"
         "  --no-prune            Do not attempt to prune duplicate "
         "operations from the\n"
         "                        graph\n"
